522f3f5942f981457f9c591b642bb92ab5816176
DtxdF <DtxdF@protonmail.com>
2021-01-29 09:37:04 -0400

    Se verifica si la carpeta ya existe para que no la cree y prosiga con la reinstalación

--- dependences.sh
+++ dependences.sh
@@ -1,5 +1,15 @@
+#!/bin/sh
+
+BUILD_FOLDER="build"
+
 # sudo apt-get install libssl-dev libsqlite3-dev libsodium-dev libargon2-dev cmake make gcc -y
-mkdir build && cd build
+
+if [ ! -e "$BUILD_FOLDER" ];then
+	mkdir "$BUILD_FOLDER"
+
+fi
+
+cd "$BUILD_FOLDER"
 export CC=`which gcc`
 cmake ..
 sudo make install
