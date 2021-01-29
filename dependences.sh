c5571dc6da648796377afc1abe25e0eb0b187aa6
DtxdF <40880686+DtxdF@users.noreply.github.com>
2021-01-29 09:10:16 -0400

    Update dependences.sh
    Se tuvo que comentar la primera línea para no excluir a las otras distribuciones que usen gestores de paquetes diferentes. No hay que eliminarla, puede servir como guía para todo aquel que use debian/debian-based.

--- dependences.sh
+++ dependences.sh
@@ -1,4 +1,4 @@
-sudo apt-get install libssl-dev libsqlite3-dev libsodium-dev libargon2-dev cmake make gcc -y
+# sudo apt-get install libssl-dev libsqlite3-dev libsodium-dev libargon2-dev cmake make gcc -y
 mkdir build && cd build
 export CC=`which gcc`
 cmake ..
