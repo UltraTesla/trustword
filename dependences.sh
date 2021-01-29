961dddec39cec491526cb21e166384cf744c469c
DtxdF <DtxdF@protonmail.com>
2021-01-29 09:33:23 -0400

    Revert "Dejar que el usuario dé los permisos administrativos correspondientes fuera del script."
    This reverts commit ed1028c3f6ee39523bdfbe807a97ef26790cbe4b.

--- dependences.sh
+++ dependences.sh
@@ -1,12 +1,5 @@
-#!/bin/sh
-
 # sudo apt-get install libssl-dev libsqlite3-dev libsodium-dev libargon2-dev cmake make gcc -y
-
-if [ ! -e build ];then
-	mkdir build
-fi
-
-cd build
+mkdir build && cd build
 export CC=`which gcc`
 cmake ..
-make install
+sudo make install
